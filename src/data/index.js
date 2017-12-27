f7afe3fd0268311f0ca6b59ef79958f13694adff
pankajladhar <ladharpankaj@gmail.com>
2017-12-27 11:53:30 +0530

    Added new style

---
+++ src/data/index.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        "projectName": "GFonts Space",
+        "githubUrl":"https://github.com/pankajladhar/GFontsSpace",
+        "demoUrl": "https://pankajladhar.github.io/GFontsSpace/",
+        "description" : "Allows user to play with available Google fonts with comparison multiple fonts."
+    },
+    {
+        "projectName": "Github Search Engine",
+        "githubUrl":"https://github.com/pankajladhar/GHSE",
+        "demoUrl": "https://pankajladhar.github.io/GHSE/",
+        "description" : "Example of Github API, when search with github username/reponame it will fetch data on one place."
+    },
+    {
+        "projectName": "Github Markdown Generator",
+        "githubUrl":"https://github.com/pankajladhar/github-markdown-generator",
+        "demoUrl": "https://pankajladhar.github.io/github-markdown-generator/",
+        "description" : "Application has WYSIWYG editor. User can use this to write there documentation, corresponding markdown(github compatible) will be auto generated"
+    },
+    {
+        "projectName": "Reactgram",
+        "githubUrl":"https://github.com/pankajladhar/Reactgram",
+        "demoUrl": "https://pankajladhar.github.io/Reactgram/#/",
+        "description" : "Clone of instagram which basic functionality of like image or add comments on image"
+    },
+    {
+        "projectName": "React Logo Generator",
+        "githubUrl":"https://github.com/pankajladhar/react-logo-generator",
+        "demoUrl": "https://pankajladhar.github.io/react-logo-generator/",
+        "description" : "An application to play with react logo to generate svg/png with different color and background color."
+    },
+    {
+        "projectName": "CSS Snippets",
+        "githubUrl":"https://github.com/pankajladhar/css-snippets",
+        "demoUrl": "https://pankajladhar.github.io/css-snippets/",
+        "description" : "Collection of some of CSS cool stuff"
+    }
+]
+
+export default data;
